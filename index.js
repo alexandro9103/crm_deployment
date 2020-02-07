c540d61c3552a50eb9d0877a382159a8f8b058e8
Alejandro Perez <alexandro9103@gmail.com>
2020-02-07 13:06:39 -0300

    probando 3

--- index.js
+++ index.js
@@ -47,6 +47,11 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true
 })
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("La base de Datos se ha conectado OK!!!");
+});
 
 
 
